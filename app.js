54cdf853f4555bc4fc3587ff70f5f1271a8332e1
chotonmanna <biswajit.manna8@gmail.com>
2023-02-27 15:35:14 +0530

    Initial commits

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+const port = 3001;
+
+app.post('/registration', (req, res) => {
+  console.log(req.body);
+ 
+  res.send('Hello World!');
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
